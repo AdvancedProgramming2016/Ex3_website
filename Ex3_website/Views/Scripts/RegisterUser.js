7641b5722cb9f185a2276d60b1b2653fdc1cfaf4
Ofir Bitron <bitrono@yahoo.com>
2017-06-23 15:18:39 +0300

    Navbar works and notify win/lose

--- Ex3_website/Views/Scripts/RegisterUser.js
+++ Ex3_website/Views/Scripts/RegisterUser.js
@@ -1,30 +1,31 @@
 ﻿jQuery(function ($) {
 
     //Load navbar.
-    $("#loginNav").load("Navbar.html");
+    $("#navBar").load("Navbar.html");
 
-    $("#submit").on('click', function () {
+    $("#submitButton").on('click', function () {
 
         if ($("#password").val() != $("#confirmPassword").val()) {
             alert("password and confirmation not identical");
         }
         else {
-            $ajax({
+
+            $.ajax({
                 type: 'POST',
                 url: '../../api/Users',
                 data: {
-                    Username: $("username").val(),
-                    Password: $("password").val(),
-                    Email: $("email").val(),
-                    GamesWon: '0',
-                    GamesLost: '0'
+                    Username: $("#username").val(),
+                    Password: $("#password").val(),
+                    Email: $("#email").val(),
+                    GamesWon: 0,
+                    GamesLost: 0
                 },
-                dataType: 'json',
                 success: function (response) {
-                    alert("User added successfully");
+                    alert("user added");
                 },
-                error: function(xhr, textStatus, errorThrown) {
-                    alert("Error registering");
+
+                error: function (xhr, textStatus, errorThrown) {
+                    alert("Error: connection lost");
                 }
             });
         }
