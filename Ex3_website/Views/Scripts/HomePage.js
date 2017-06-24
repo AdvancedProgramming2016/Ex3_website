74d76a12f125ba3fdddced516c6ebb804628823e
Ofir Bitron <bitrono@yahoo.com>
2017-06-24 13:16:54 +0300

    homepage + navbar JS files

---
+++ Ex3_website/Views/Scripts/HomePage.js
@@ -0,0 +1,12 @@
+﻿jQuery(function ($) {
+
+    var defaultNumOfRows = localStorage.getItem("defaultNumOfRows");
+
+    if (!defaultNumOfRows) {
+
+        // Set the default values.
+        localStorage.setItem("defaultNumOfRows", "15");
+        localStorage.setItem("defaultNumOfCols", "15");
+        localStorage.setItem("defaultAlgo", "Dfs");
+    }
+});
---
+++ Ex3_website/Views/Scripts/Navbar.js
@@ -0,0 +1,21 @@
+﻿jQuery(function ($) {
+
+    var username = sessionStorage.getItem("username");
+
+    if (username != null) {
+        $("#register").innerHTML = username;
+        $("#register").href = "";
+        $("#login").innerHTML = "Log off";
+        $("#login").href = "Login.html";
+    }
+
+    $("#login").click(function () {
+
+        // The user is logged in and wants to log off.
+        if (sessionStorage.getItem("username") != null) {
+
+            // Clear all session storages.
+            sessionStorage.clear();
+        }
+    });
+});
